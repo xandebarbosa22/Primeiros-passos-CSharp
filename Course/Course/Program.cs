d00f70db7c7c6bf035b44ff600de0322098da357
Alexandre <alexandrebarbosa8371@gmail.com>
2025-05-30 09:45:21 -0300

    Meus primeiros passos em C#.

---
+++ Course/Course/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            char genero = 'f';
+            char letra = '\u0041';
+            sbyte x = 100;
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+            float n5 = 4.5f;
+            double n6 = 4.5;
+            string nome = "Maria Green"; // String é uma cadeia de caracteres unicode IMUTÁVEL! (segurança, simplicidade, thread safe)
+            object obj1 = "Alexandre";
+            object obj2 = 4.5f;
+
+            Console.WriteLine(completo);
+            Console.WriteLine(genero);
+            Console.WriteLine(letra);
+            Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            Console.WriteLine(n5);
+            Console.WriteLine(n6);
+            Console.WriteLine(nome);
+            Console.WriteLine(obj1);
+            Console.WriteLine(obj2);
+        }
+    }
+}

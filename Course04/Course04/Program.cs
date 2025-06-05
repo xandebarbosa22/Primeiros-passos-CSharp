9188de3f5166061e863958cb8cb68cdbd64f904d
Alexandre <alexandrebarbosa8371@gmail.com>
2025-06-05 19:41:10 -0300

    Aprendendo sobre conversão implícita e casting

---
+++ Course04/Course04/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Course04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Conversão implícita entre tipos de variáveis:
+
+            float a = 4.5f;
+            double b = a;
+
+            Console.WriteLine(b);
+
+            // Casting -> Conversão explícita entre tipos de variáveis COMPATÍVEIS:
+
+            double c = 2.5;
+            float d = (float) c;
+
+            Console.WriteLine(d);
+
+            int e = (int) c;        // Quando convertemos uma variável float ou double para int, os valores decimais vão ser "Truncados"
+
+            Console.WriteLine(e);
+
+            int val1 = 5;
+            int val2 = 2;
+            double res = (double) val1 / val2;      // Se não usar o "Casting", o compilador só vai fazer o cálculo dos números inteiros.
+
+            Console.WriteLine(res);
+
+
+        }
+    }
+}

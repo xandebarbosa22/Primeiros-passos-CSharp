082d2662b8709136a18f274f1b0bdb525da38df9
Alexandre <alexandrebarbosa8371@gmail.com>
2025-06-19 20:11:59 -0300

    Entrada de Dados em C# - Parte 2

---
+++ Course07/Course07/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            char ch = char.Parse(Console.ReadLine());
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);     // Só com isso, o compilador ainda vai mostrar com a ",".
+            string[] i = Console.ReadLine().Split(' ');
+            string nome = i[0];
+            char sexo = char.Parse(i[1]);
+            int idade = int.Parse(i[2]);
+            double altura = double.Parse(i[3], CultureInfo.InvariantCulture);
+
+
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));             // Dessa forma, os números decimais serão apresentados com ".".
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}

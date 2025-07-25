a5c7101fc4a22fffc8ec6d4001b8ecc5649db363
Alexandre <alexandrebarbosa8371@gmail.com>
2025-07-25 20:00:42 -0300

    Iniciando no estudo sobre funções.

---
+++ Course09/Course09/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 números inteiros:");
+
+            int num1, num2, num3;
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            num3 = int.Parse(Console.ReadLine());
+
+            int resultado = funcMaior(num1, num2, num3);
+
+            Console.WriteLine($"O maior número é: {resultado}");
+        }
+
+        static int funcMaior(int a, int b, int c)
+        {
+            int maior;
+            if (a >= b && a >= c)
+            {
+                maior = a;
+            }
+            else if (b > c)
+            {
+                maior = b;
+            }
+            else
+            {
+                maior = c;
+            }
+            return maior;
+        }
+    }
+}

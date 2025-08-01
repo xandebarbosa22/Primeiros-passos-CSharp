21ce304fbad74d976a9488891ecbb0abce6862d2
Alexandre <alexandrebarbosa8371@gmail.com>
2025-08-01 17:33:35 -0300

    Aula de introdução à OOP.

---
+++ Course10/Course10/Triangulo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course10
+{
+    internal class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}

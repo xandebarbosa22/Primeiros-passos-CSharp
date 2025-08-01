3eb103d0d5abd693782311d03eea2aca8231b6c0
Alexandre <alexandrebarbosa8371@gmail.com>
2025-07-31 21:08:13 -0300

    Exercício com a função FOR.

---
+++ Exercise04/Exercise04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Net.NetworkInformation;
+
+namespace Exercise04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro entre 1 e 1000: ");
+            int num = int.Parse(Console.ReadLine());
+            int impares = 0;
+
+            Console.WriteLine("-------------------");
+
+            for (int i = 1; i > 0 && i <= num; i++)
+            {
+                if ((i % 2) != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
